1eb5d4e87a5d0abf9de2da442de210acbc974ed5
CazinSacha <cazin.lep@gmail.com>
2025-01-26 18:48:26 +0100

    Collision pique bulle

--- scenes/spike_body.gd
+++ scenes/spike_body.gd
@@ -3,9 +3,15 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	connect("body_entered", Callable(self, "_on_rigidbody_collision"))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_rigidbody_collision(body):
+	if body is RigidBody2D:
+		if body.is_in_group("bubbles"):
+			get_tree().quit()
+			
