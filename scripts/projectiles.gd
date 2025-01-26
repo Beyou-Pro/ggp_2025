6e590a4966804955dced9b926fa854c75870d5da
Beyou <corentin.beyries@outlook.com>
2025-01-26 10:49:20 +0100

    added laser signal from tutorial

--- scripts/projectiles.gd
+++ scripts/projectiles.gd
@@ -1,16 +1,16 @@
 extends Node2D
 var shoot = false
 
-# Called when the node enters the scene tree for the first time.
+signal laser
+
 func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-		if shoot  == true:
-			print("feu") 
-		
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("Shoot"):
+		laser.emit()
+
 
 func _input(event):
 	if event is InputEventMouseButton:
