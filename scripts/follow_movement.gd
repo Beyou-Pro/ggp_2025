712aa9e792e5ee709a8d735563477cd17c1ced2c
CazinSacha <cazin.lep@gmail.com>
2025-01-26 04:05:35 +0100

    Ajout spikes

--- scripts/follow_movement.gd
+++ scripts/follow_movement.gd
@@ -18,17 +18,19 @@ var angle_bubble
 func _process(delta: float) -> void:
 	Mouse = get_local_mouse_position()
 	bubble_center = bubble.global_position + bubble.offset
-	angle_bubble = bubble_center.angle_to_point(cursor.global_position)
-	rotation = angle_bubble
 	cursor.global_position = cursor.get_global_mouse_position()
+	angle_bubble = bubble_center.angle_to_point(cursor.global_position)
+	rotation = angle_bubble	
 	#bubble.position = Vector2.ZERO
 	bubble.rotation = -rotation
+	
+	print(bubble.global_position)
 
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			bubble.global_position += Vector2(10,0)
-			print(bubble.position)
+			
 	
 	
 	
