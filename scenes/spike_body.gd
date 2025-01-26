34943930ff3c12ef6342e71df383afd24151d5c9
CazinSacha <cazin.lep@gmail.com>
2025-01-26 19:30:54 +0100

    dzadazdazdazd

--- scenes/spike_body.gd
+++ scenes/spike_body.gd
@@ -6,12 +6,10 @@ func _ready() -> void:
 	connect("body_entered", Callable(self, "_on_rigidbody_collision"))
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+
 
 func _on_rigidbody_collision(body):
-	if body is RigidBody2D:
-		if body.is_in_group("bubbles"):
-			get_tree().quit()
+	if body.is_in_group("bubbles"):
+		get_tree().quit()
 			
