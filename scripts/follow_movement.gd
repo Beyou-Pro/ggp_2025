b081e29c74a85b758fffa8dbaaa3abadea855c7f
CazinSacha <cazin.lep@gmail.com>
2025-01-26 03:10:11 +0100

    uyazgzduyazegfezu

--- scripts/follow_movement.gd
+++ scripts/follow_movement.gd
@@ -21,8 +21,14 @@ func _process(delta: float) -> void:
 	angle_bubble = bubble_center.angle_to_point(cursor.global_position)
 	rotation = angle_bubble
 	cursor.global_position = cursor.get_global_mouse_position()
-	bubble.position = Vector2(2,0)
+	bubble.position = Vector2.ZERO
 	bubble.rotation = -rotation
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			bubble.position += Vector2(10,0)
+			print(bubble.position)
 	
 	
 	
